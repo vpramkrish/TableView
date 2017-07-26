275b048a8378226d33f42dfff3fd7f83091c0d4c
C Ramkumar <ramgcecse@gmail.com>
2017-07-26 14:57:39 -0700

    chg

--- SampleApp/ViewController.swift
+++ SampleApp/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("cank")
         
     }
     override func didReceiveMemoryWarning() {
